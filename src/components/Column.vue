e293243c11154bdd6fb4ba0eb0b88fc115763312
ma <92aggerholm@gmail.com>
2021-11-19 09:42:30 +0100

    project files

---
+++ src/components/Column.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="col">
+    <div class="title">
+        <span>{{columntitle}}</span>
+    </div>
+  <column-box v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.text"></column-box>
+</div>
+</template>
+
+<script>
+import ColumnBox from '@/components/ColumnBox.vue'
+export default {
+    components: {ColumnBox},
+      name: 'Column',
+      props: [
+          "todos",
+          "columntitle",
+      ]
+}
+</script>
+
+<style>
+.col {
+    color: rgb(0, 0, 0);
+    height: 100%;
+    width: 20%;
+    display: inline-block;
+    margin: 1%;
+    vertical-align: top;
+    padding-bottom: 25px;
+    border: solid black 1px;
+}
+.col:nth-of-type(1) > div:not(:first-of-type) {
+    background: #b1b1b1;
+}
+.col:nth-of-type(2) > div:not(:first-of-type) {
+    background: #cf8f8f;
+}
+.col:nth-of-type(3) > div:not(:first-of-type) {
+    background: #8fc6cf;
+}
+.col:nth-of-type(4) > div:not(:first-of-type) {
+    background: #8fcfa0;
+}
+.title{
+    margin-top: 20px;
+    font-size: 120%;
+    font-style: italic;    
+}
+
+.title span {
+    top: -20px;
+    width: 0; 
+    height: 0; 
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+}
+</style>
---
+++ src/components/ColumnBox.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="Task">
+    {{todo.id}}
+    {{todo.text}}
+</div>
+</template>
+
+<script>
+export default {
+    props: [
+        "todo"
+    ]
+}
+</script>
+
+<style>
+.Task {
+    width: 90%;
+    height: 150px;
+    border: solid white 1px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 16px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-content: center;
+}
+</style>
